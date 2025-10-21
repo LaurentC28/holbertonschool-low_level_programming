5a5de2606f92f98e188572e3ca490f8b277d9935
LaurentC28 <12125@holbertonstudents.com>
2025-10-21 19:13:29 +0000

    prend le dernier chiffre d'un nombre aleatoire et imprime un commentaire different en fonction de sa valeur

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,26 +4,28 @@
 /**
  * main - Entry point
  *
- * Description: displays the last digit of a random number
+ * Description: Define if number is negative or positive
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        int m = n % 10;
+int n;
+int m;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+m = n % 10;
 if (m > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, m);
+printf("Last digit of %d is %d and is greater than 5\n", n, m);
 }
 else if (m == 0)
 {
-printf("Last digit of %d is %d and is 0", n, m);
+printf("Last digit of %d is %d and is 0\n", n, m);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, m);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 }
-        return (0);
+return (0);
 }
