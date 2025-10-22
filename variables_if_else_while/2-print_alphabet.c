53bb439e286a11044c5181f6f00309e164c7a2b2
LaurentC28 <12125@holbertonstudents.com>
2025-10-22 07:54:02 +0000

    prints the alphabet in lowercase, followed by a new line

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lettre = 'a';
+
+do {
+putchar(lettre);
+putchar('\n');
+lettre++;
+} while (lettre <= 'z');
+
+return (0);
+}
