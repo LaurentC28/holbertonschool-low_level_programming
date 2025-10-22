b0fa4d19f40d471b7749638fb089cadd89a6e594
LaurentC28 <12125@holbertonstudents.com>
2025-10-22 13:41:50 +0000

    Display all single digit numbers of base 10 without printf

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Display all single digit numbers of base 10.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int chiffre;
+
+while (chiffre <= 9)
+{
+putchar(chiffre);
+chiffre++;
+}
+putchar('\n');
+return (0);
+}
