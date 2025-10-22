05ac75997a811de75d2c198c422daabd28797019
LaurentC28 <12125@holbertonstudents.com>
2025-10-22 08:55:53 +0000

    prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char lettre = 'a';
+char lettre2 = 'A';
+do {
+putchar(lettre);
+lettre++;
+} while (lettre <= 'z');
+
+do {
+putchar(lettre2);
+lettre2++;
+} while (lettre2 <= 'Z');
+
+
+putchar('\n');
+return (0);
+}
