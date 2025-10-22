4ed484e7ba1dfbf394b08b716a74bc85a63a4ec0
LaurentC28 <12125@holbertonstudents.com>
2025-10-22 11:58:49 +0000

    Write a program that prints the alphabet in lowercase except q and e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 char lettre = 0;
-for (char lettre = 'a'; lettre <= 'z'; lettre++)
+for (lettre = 'a'; lettre <= 'z'; lettre++)
 if (lettre == 'q' || lettre == 'e')
 {
 continue;
