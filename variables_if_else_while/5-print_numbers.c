a590ee0c41c01fb705bb00a2ce7f7f9cea861a21
LaurentC28 <12125@holbertonstudents.com>
2025-10-22 13:35:57 +0000

    affiche les chiffres de 0 a 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Display all single digit numbers of base 10.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int chiffre;
+
+while (chiffre <= 9)
+{
+printf("%d", chiffre);
+chiffre++;
+}
+return (0);
+}
---
+++ variables_if_else_while/CBase.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Description on the command.
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+ */le code va ici*/
+return (0);
+}
