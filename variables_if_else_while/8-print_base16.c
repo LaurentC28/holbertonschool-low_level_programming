f245b498eec8c736bc7b0fdfdbc6aeffa4333f45
LaurentC28 <12125@holbertonstudents.com>
2025-10-23 09:28:30 +0000

    affiche 123456789abcdef avec la cde putchar

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char chiffre;
+char lettre;
+
+for (chiffre = 48 ; chiffre <= 57 ; chiffre++)
+{
+putchar(chiffre);
+}
+for (lettre = 'a' ; lettre <= f ; lettre++)
+{
+putchar(lettre);
+}
+
+putchar('\n');
+return (0);
+}
