bf56bfad202a24cf202a5ccda9cd4a2fa621304b
LaurentC28 <12125@holbertonstudents.com>
2025-10-23 21:08:59 +0000

    Test tache0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 int main(void) {
-    for (int i = 0; message[i] != '\0'; i++) {
+    int = i
+    for (i = 0; message[i] != '\0'; i++) {
         putchar(message[i]);
     }
 
