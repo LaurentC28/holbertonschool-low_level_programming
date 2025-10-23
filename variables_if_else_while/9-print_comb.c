5b9cecead3c75ce9d6f92b0a62ed9edfdc4b59f7
LaurentC28 <12125@holbertonstudents.com>
2025-10-23 09:50:21 +0000

    affiche la combinaison des 10 chiffres avec seulement 4 putchar

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints  all possible combinations of single-digit numbers..
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char chiffre;
+
+for (chiffre = 48 ; chiffre <= 57 ; chiffre++)
+{
+putchar(chiffre);
+putchar (',');
+putchar (' ');
+}
+
+putchar('\n');
+return (0);
+}
