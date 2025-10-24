b5f87d363afb8e663628269097b0d68b33eef365
LaurentC28 <12125@holbertonstudents.com>
2025-10-24 12:43:03 +0000

    donne la valeur absolue d'un entier

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _abs - description gives the absolute value of an integer.
+ * description - for parameter or int 'n'
+ * @n: explains in "betty" that we are working on the variable n
+ * Return: void
+ */
+int _abs(int n)
+{
+return (_abs(n));
+}
