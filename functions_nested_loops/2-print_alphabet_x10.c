05114edd3e29023a71bcfd7a1a4144faa38fd688
LaurentC28 <12125@holbertonstudents.com>
2025-10-24 08:44:34 +0000

    affiche 10x l'alphabet avec les fonctions crees de toutes piece

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char lettre;
+char repetition;
+for (repetition = 0 ; repetition <= 10 ; repetition++)
+{
+        for (lettre = 'a'; lettre <= 'z'; lettre++)
+        {
+        _putchar(lettre);
+        }
+}
+_putchar('\n');
+}
