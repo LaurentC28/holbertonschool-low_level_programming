a56c35a1590b310de9615b8c702333f58d741e72
LaurentC28 <12125@holbertonstudents.com>
2025-10-24 11:33:55 +0000

    dit si un caractere est une lettre alphabetique

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - description checks for alphabetic character
+ * description - for parameter or int 'c'
+ * @c: explains in "betty" that we are working on the variable c
+ * Return: void
+ */
+int _isalpha(int c)
+{
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
