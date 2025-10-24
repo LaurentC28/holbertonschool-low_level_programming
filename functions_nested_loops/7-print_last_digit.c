bc0073680d6992ed1ff0fd0fb3e1b1149a6468e4
LaurentC28 <12125@holbertonstudents.com>
2025-10-24 13:24:54 +0000

    affiche le dernier chiffre d'un nombre

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_last_digit - description gives the last digit of a number.
+ * description - for parameter or int 'n'
+ * @n: explains in "betty" that we are working on the variable n
+ * Return: void
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	{
+	return (n);
+	}
+}
