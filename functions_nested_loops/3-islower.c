81f1273dd92f3b378be08389587ac6b2fdf4ee5c
LaurentC28 <12125@holbertonstudents.com>
2025-10-24 10:10:57 +0000

    dit si une lettre est majuscule ou minuscule et renvoi 1 ou 0 en fonction

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * checks for lowercase character
+ *
+ * Return: void
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+    return(1);
+}
+else
+{
+    return(0);
+}
+}
