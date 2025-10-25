625a079ecf91d5a710c2b791b238a5bbdb637831
LaurentC28 <12125@holbertonstudents.com>
2025-10-25 21:05:06 +0000

    affiche les resultat de la table de 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -2,34 +2,34 @@
 
 void times_table(void)
 {
-    int i, j, product, next;
+	int i, j, product, next;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            product = i * j;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
 
-            if (product >= 10)
-            {
-                _putchar(product / 10 + '0');
-                _putchar(product % 10 + '0');
-            }
-            else
-            {
-                _putchar(product + '0');
-            }
+			if (product >= 10)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else
+			{
+				_putchar(product + '0');
+			}
 
-            if (j < 9)
-            {
-                next = i * (j + 1);
-                _putchar(',');
-                _putchar(' ');
-                if (next < 10)
-                    _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+			if (j < 9)
+			{
+				next = i * (j + 1);
+				_putchar(',');
+				_putchar(' ');
+				if (next < 10)
+					_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 
 }
