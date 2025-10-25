226188491bb937755431a87094bc5f9a845ba9b9
LaurentC28 <12125@holbertonstudents.com>
2025-10-25 20:46:13 +0000

    affiche les resultat de la table de 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,16 +1,8 @@
-#include "main.h"
-#include <ctype.h>
+#include <main.h>
 
-/**
- * jack_bauer - print the Jack Bauer time.
- * description - for parameter or int 'n'
- * Return: void
- */
-
-void times_table(void)
+int main(void)
 {
-int i, j;
-    int product;
+    int i, j, product;
 
     for (i = 0; i <= 9; i++)
     {
@@ -18,20 +10,30 @@ int i, j;
         {
             product = i * j;
 
-if (product < 10)
-{
-    putchar(' ');
-    putchar(' ');
-    putchar(product + '0');
-}
-else
-{
-    putchar(' ');
-    putchar(product / 10 + '0');
-    putchar(product % 10 + '0');
-}
+            /* espace d'alignement */
+            if (product < 10)
+                _putchar(' ');
+
+            /* afficher le nombre */
+            if (product >= 10)
+            {
+                _putchar(product / 10 + '0');
+                _putchar(product % 10 + '0');
+            }
+            else
+            {
+                _putchar(product + '0');
+            }
+
+            /* ajouter virgule et espace sauf à la fin */
+            if (j < 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
         }
         _putchar('\n');
     }
 
+    return 0;
 }
