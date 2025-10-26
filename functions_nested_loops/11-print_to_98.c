ca9cc04ea92ee6b9890fdcce029f58d8ea22eed2
LaurentC28 <12125@holbertonstudents.com>
2025-10-26 21:22:18 +0000

    fichu derniere tache !

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,30 +3,30 @@
 /**
  * main - Entry point
  *
- * Description: Description
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: explains in "betty" that we are working on the variable n
  * Return: Always 0 (Success)
  */
 void print_to_98(int n)
 {
-    if (n <= 98)
-    {
-        for (; n <= 98; n++)
-        {
-            printf("%d", n);
-            if (n != 98)
-                printf(", ");
-        }
-    }
-    else
-    {
-        for (; n >= 98; n--)
-        {
-            printf("%d", n);
-            if (n != 98)
-                printf(", ");
-        }
-    }
-    printf("\n");
-	return;
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	printf("\n");
 }
 
