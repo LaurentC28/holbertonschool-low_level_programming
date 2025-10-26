09cb1a2b57561ec53a3fa95418796b9dec327586
LaurentC28 <12125@holbertonstudents.com>
2025-10-26 19:58:11 +0000

    fonction qui additionne deux int

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - addition two variables.
+ * description - for parameter or int 'n'
+ * @n: explains in "betty" that we are working on the variable n
+ * @m: explains in "betty" that we are working on the variable m
+ * Return: void
+ */
+int add(int n, int m)
+{
+int result = n + m;
+
+_putchar (result + '0');
+return (0);
+}
