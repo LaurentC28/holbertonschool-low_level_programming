8ce21552ad4092a5fb9a058e56bee076d47f0dbf
LaurentC28 <12125@holbertonstudents.com>
2025-10-27 12:16:37 +0000

    sauvegarde tache 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -4,8 +4,7 @@
  * _isupper - checks for uppercase character.
  * @n: working variable.
  *
- * Description: _isupper returns 1 if n is uppercase o otherwise.
- * return: 1 if true 0 if false.
+ * Return: 1 if true 0 if false.
  */
 
 int _isupper(int n)
