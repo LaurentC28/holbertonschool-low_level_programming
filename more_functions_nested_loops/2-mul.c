93a71aca775f7b25f95d399b876764d3e758613b
LaurentC28 <12125@holbertonstudents.com>
2025-10-27 12:58:04 +0000

    fonctin por multiplier 2 entiers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplication two variables.
+ * description - for parameter or int 'n,m'
+ * @n: explains in "betty" that we are working on the variable n
+ * @m: explains in "betty" that we are working on the variable m
+ * Return: void
+ */
+int mul(int n, int m)
+{
+return (n * m);
+}
