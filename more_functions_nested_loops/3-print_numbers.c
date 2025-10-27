b767fdd154c3db771af4d40a78c91a07e665e3ca
LaurentC28 <12125@holbertonstudents.com>
2025-10-27 13:18:53 +0000

    fonction qui affiche les chiffre suivi d'un retour a la ligne

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - display the digits.
+ *
+ * Return: 1 if true 0 if false.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (; n <= 9 ; n++)
+	{
+	_putchar(n + 48);
+	}
+}
