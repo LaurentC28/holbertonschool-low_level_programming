3bbd62d6556c28c7535bc2926104403f6be773ae
LaurentC28 <12125@holbertonstudents.com>
2025-10-27 15:28:37 +0000

    affiche les nombres de 0 a 14 10 fois

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,17 @@
 
 void more_numbers(void)
 {
-    int n;
-    int m;
+	int n;
+	int m;
 
-    for (n = 0 ; n < 10 ; n++)
-    {
-        for (m = 0 ; m <= 14 ; m++)
-        {
-            if (m > 9)
-            _putchar (m / 10 + 48);
-            _putchar (m % 10 + 48);
-        }
-    _putchar('\n');
-    }
+	for (n = 0 ; n < 10 ; n++)
+	{
+		for (m = 0 ; m <= 14 ; m++)
+		{
+			if (m > 9)
+			_putchar (m / 10 + 48);
+			_putchar (m % 10 + 48);
+		}
+	_putchar('\n');
+	}
 }
