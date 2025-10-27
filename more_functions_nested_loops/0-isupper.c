6f7288f642182eac790223233e2a39f875b131fb
LaurentC28 <12125@holbertonstudents.com>
2025-10-27 10:56:21 +0000

    sauvegarde tache 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _isupper - checks for uppercase character.
+ * @n: working variable.
+ *
+ * Description: Returns 1 if c is uppercase o otherwise.
+ */
 
 int _isupper(int n)
 {
