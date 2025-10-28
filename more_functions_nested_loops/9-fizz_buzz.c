6b42f02373893d3c34ecb2fefbf6df2f027654d1
LaurentC28 <12125@holbertonstudents.com>
2025-10-28 13:28:19 +0000

    programme fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,36 +3,37 @@
 #include <stdlib.h>
 /**
  * fizz_buzz - Write a program that prints the numbers from 1 to 100.
- *
+ * description - the multiple of 3 & 5 replaced.
+ * main - Programm Entry
  * Return: return.
  */
 
-int main (void)
+int main(void)
 {
 int i;
 
 for (i = 1 ; i <= 100 ; i++)
 {
 
-    if (i % 3 == 0 && i % 5 == 0)
-    {
-    printf("FizzBuzz");
-    }
-    else if(i % 5 == 0)
-    {
-    printf("Buzz");  
-    }
-    else if(i % 3 == 0)
-    {
-    printf("Fizz");    
-    }
-    else
-    {
-    printf("%d",i);
-    }
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else if (i % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else
+	{
+	printf("%d", i);
+	}
 
-    if (i < 100)
-    printf(" ");
+	if (i < 100)
+	printf(" ");
 }
 printf("\n");
 return (0);
