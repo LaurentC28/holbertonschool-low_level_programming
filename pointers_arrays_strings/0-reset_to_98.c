e3f4ab61d9b28835bfac28ea10f81ddd97854e34
LaurentC28 <12125@holbertonstudents.com>
2025-10-29 12:55:59 +0000

    tache 0 prends un pointeur vers un int comme parametre et mets a jours la valeur vers laquelle il pointe

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - Write a function that takes a pointer
+ * to an int as parameter and updates the value it points to to 98.
+ * @n: explains in "betty" that we are working on the variable n
+ *
+ * Return: no return.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
