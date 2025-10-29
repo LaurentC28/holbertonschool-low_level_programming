5059025a59047a39c218b0c0d0eadb08dddb4aa8
LaurentC28 <12125@holbertonstudents.com>
2025-10-29 15:22:17 +0000

    imprime une chaine de caracteres

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -9,13 +9,14 @@
 */
 
 void _puts(char *str)
-{ 
-    int i = 0;
-    while (str[i] != '\0')
-    {
-    _putchar(str[i]);
-    str++;
-    }
-    _putchar('\n');
+{
+	int i = 0;
+	
+	while (str[i] != '\0')
+	{
+	_putchar(str[i]);
+	str++;
+	}
+	_putchar('\n');
 }
 
