ae097769122d2d8c4c269ace956d730ba02c9ca6
LaurentC28 <12125@holbertonstudents.com>
2025-10-29 13:14:44 +0000

    tache 1 echange la valeur de 2 int en utilisant leurs adresse

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - swaps the values of two integers
+ * @n: explains in "betty" that we are working on the variable n
+ *
+ * Return: no return.
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp = 0;
+
+temp = *b; //la variable temp "prend" la valeur de l'adresse de b
+*b = *a; //la valeur de l'adresse de b prends la valeur de l'adresse de a
+*a = temp;
+}
