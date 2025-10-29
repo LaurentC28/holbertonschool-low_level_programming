fe2771efb86dcba67c40e1991e5ea7a311a58b06
LaurentC28 <12125@holbertonstudents.com>
2025-10-29 15:19:30 +0000

    imprime une chaine de caracteres

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - Write a function that takes a pointer
+* and print a string.
+* @s: explains in "betty" that we are working on the variable s
+*
+* Return: no return.
+*/
+
+void _puts(char *str)
+{ 
+    int i = 0;
+    while (str[i] != '\0')
+    {
+    _putchar(str[i]);
+    str++;
+    }
+    _putchar('\n');
+}
+
