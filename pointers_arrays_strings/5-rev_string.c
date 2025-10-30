c5f47b35cc2af8c2fe290790c7a5085a141d45e8
LaurentC28 <12125@holbertonstudents.com>
2025-10-30 11:39:00 +0000

    affiche la chaine de caractere renversee

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rev_string - Write a function that takes a pointer that prints a string.
+* in reverse.
+* @str: explains in "betty" that we are working on the variable s
+*
+* Return: no return.
+*/
+
+void rev_string(char *s)
+{
+	char *debut = s;
+	char *fin = s;
+	char temp=0;
+
+	while (*fin != '\0')
+	{
+		fin++;
+	}
+	fin--;
+	while (debut < fin)
+	{
+	temp = *fin;
+	*fin = *debut;
+	*debut = temp;
+
+	debut++;
+	fin--;
+	}
+	
+}
