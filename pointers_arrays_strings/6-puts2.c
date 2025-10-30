30d06057aa28cf8790257c4cf1e1b29db0a448fc
LaurentC28 <12125@holbertonstudents.com>
2025-10-30 12:39:29 +0000

    fonction qui affiche 02468 au lieu de 123456789

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* rev_string - Write a function that prints every other character of a string,
+* starting with the first character, followed by a new line.
+* @str: explains in "betty" that we are working on the variable str.
+*
+* Return: no return.
+*/
+
+void puts2(char *str)
+{
+    while (*str != *str % 2)
+    {
+        _putchar(*str);
+        str = str + 2;
+    }
+_putchar('\n');
+}
