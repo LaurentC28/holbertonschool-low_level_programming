8433d2a59ef5c7e453d7b4e3e9a3bde7fc4b8bff
LaurentC28 <12125@holbertonstudents.com>
2025-10-31 11:58:03 +0000

    convertit une chaine de carateres en entiers

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _atoi - Write a function that convert a string to an integer.
+* @s: explains in "betty" that we are working on the pointer dest.
+* Return: no return.
+*/
+
+int _atoi(char *s)
+{
+    int i = 0;
+    int signe = 1;
+    int nombre = 0;
+
+    while (s[i] == ' ')
+    {
+        i++;
+    }
+    if (s[i] == '+' || s[i] == '-')
+    {
+        if (s[i] == '-')
+        signe = -1;
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        nombre = nombre * 10 + (s[i] - '0');
+        i++;
+    }
+    nombre = nombre * signe;
+    return (nombre * signe);
+}
