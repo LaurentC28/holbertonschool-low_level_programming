d3d02591112b3b30f666cd52a5a21cc4460bf0c2
LaurentC28 <12125@holbertonstudents.com>
2025-10-31 08:17:04 +0000

    affiche 02468 au lieu de 0123456789

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* rev_string - Write a function that prints every other character of a string,
+* puts2 - Write a function that prints every other character of a string,
 * starting with the first character, followed by a new line.
 * @str: explains in "betty" that we are working on the variable str.
 *
