754dc9194eba866a90c87ed57f8cfb48892263d6
LaurentC28 <12125@holbertonstudents.com>
2025-11-01 11:26:11 +0000

    tests  de tableaux  en C

---
+++ tests/0-fcttest.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* print_array - Write a function that prints n elements of an
+* array of integers, followed by a new line.
+* @a: explains in "betty" that we are working on the pointer a.
+* @n: explains in "betty" that we are working on the pointer n.
+* Return: no return.
+*/
+
+void fonctLol(int *a, int n)
+{
+
+}
