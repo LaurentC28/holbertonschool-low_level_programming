b03174e0e715275392bc0e82437a701e1de6893d
LaurentC28 <12125@holbertonstudents.com>
2025-11-03 20:25:25 +0000

    fonction qui trouve une sous chaine

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -10,19 +10,17 @@ char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack != '\0')
 	{
-		if (*haystack == *needle)
-		{
-			char *h = haystack;
-			char *n = needle;
 
-			while (*h == *n && *n != '\0')
-			{
-				h++;
-				n++;
-			}
-			if (*n == '\0')
-			return (haystack);
+		char *h = haystack;
+		char *n = needle;
+
+		while (*h == *n && *n != '\0')
+		{
+			h++;
+			n++;
 		}
+		if (*n == '\0')
+		return (haystack);
 		haystack++;
 	}
 	return (NULL);
