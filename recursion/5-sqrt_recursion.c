f109669cceaee5080d51d80c13c906ecf686d4e0
LaurentC28 <12125@holbertonstudents.com>
2025-11-04 16:52:55 +0000

    retourne la racine carre d'un nombre

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -19,12 +19,12 @@ int compteur(int n, int i)
 	{
 		return (compteur(n, i + 1));
 	}
+}
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to find the square root of
  * Return: natural square root of n, -1 if none exists
  */
-}
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
