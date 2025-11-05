2f315c512e64aee216a9605e41f1aacff7c58740
LaurentC28 <12125@holbertonstudents.com>
2025-11-05 13:15:33 +0000

    fonction qui imprime tous les arguments qu'elle recoit

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function to print all arguments it receives.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
