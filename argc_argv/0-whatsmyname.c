cc68e715ec54c67f9dbf96ce186be58b0d4b6b98
LaurentC28 <12125@holbertonstudents.com>
2025-11-05 12:42:56 +0000

    programme qui ecrit le 1er argv de la fonction main a savoir son nom

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
-
+/**
+ * main - function to print is first argument (his name).
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s", argv[0]);
 	printf("\n");
-	return(0);
+	return (0);
 }
