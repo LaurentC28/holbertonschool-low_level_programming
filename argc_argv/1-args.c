9e30490f452448684f3d2a0198189c45ee0c44be
LaurentC28 <12125@holbertonstudents.com>
2025-11-05 12:55:09 +0000

    fonction qui imprime le nombre d'arguments

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function to print the number of argument.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d", argc);
+	printf("\n");
+	return (0);
+}
