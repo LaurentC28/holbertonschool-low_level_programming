1d1de247b71028ffc6dec3942dfdb514ccb97667
LaurentC28 <12125@holbertonstudents.com>
2025-11-05 13:59:04 +0000

    programme qui utilises ses arguments pour multiplier 2 entiers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - program that multiplies two numbers.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, prod;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	prod = a * b;
+
+	printf("%d\n", prod);
+	return (0);
+}
